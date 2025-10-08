0b8f217af42df62ea522ce78026906b9cf875bc9
zecacristina634 <zecacristina634@gamil.com>
2025-10-08 15:50:23 +0300

    seminar1

---
+++ seminar1/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.static(__dirname + './public'))
+
+app.get('/',(req,res)=>{
+    res.send('Hello world')
+})
+
+app.get('/ping',(req,res)=>{
+    res.send("pong")
+})
+
+app.listen(8080)
+
